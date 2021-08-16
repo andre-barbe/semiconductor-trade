0cc217b627d163d9f5829c7bad442edbb122acac
Andre Barbe <31875393+andre-barbe@users.noreply.github.com>
2021-08-16 19:14:01 -0400

    Load VLSI data and create graphs showing Production and Exports for each country

--- main.R
+++ main.R
@@ -21,6 +21,10 @@
 
     #Rename variables
         names(data_comtrade)[names(data_comtrade)=="Trade.Value..US.."]="TradeValue"
+    
+    #Delete non-export data
+        data_comtrade <- subset(data_comtrade, (Trade.Flow %in% c("Export","Exports")))
+        
       
 #Load CEPII Data
   #Description
@@ -47,34 +51,33 @@ library("ggrepel")
         #example of how to both do bar chart and how to subset a dataframe is from
           #https://www.datanovia.com/en/blog/how-to-subset-a-dataset-when-plotting-with-ggplot2/
       #delete data not in filter
-        subsets1 <- subset(data_comtrade, (Trade.Flow %in% c("Export","Exports") & Commodity.Code %in% c(filterto_Commodity.Code)))
+        data_graph_comtrade <- subset(data_comtrade, (Commodity.Code %in% c(filterto_Commodity.Code)))
           #NTS: annual data calls it export (no -S)
           #NTS: monthly data calls it exports (yes -S)
         #Determine top exporting coutnries
           #sort data
-            top_countries <- subsets1[subsets1$date == max(subsets1$date),]
+            top_countries <- data_graph_comtrade[data_graph_comtrade$date == max(data_graph_comtrade$date),]
               #filter to most recent year only
             top_countries <- top_countries[order(-top_countries$TradeValue),]
               #sort by trade value, descending
             top_X_countries <- top_countries$Reporter[1:4]
       #delete data not from top countries
-          #subsets1 <- subset(subsets1, (Reporter %in% top_X_countries))
+          #data_graph_comtrade <- subset(data_graph_comtrade, (Reporter %in% top_X_countries))
       #collapse data not from top 4 countries
         #if not in top 4, change name to "rest of world"
-          subsets1$Reporter[!(subsets1$Reporter %in% top_X_countries)] <- "Rest of world"
+          data_graph_comtrade$Reporter[!(data_graph_comtrade$Reporter %in% top_X_countries)] <- "Rest of world"
         #Delete all columns except for the ones I am using
-          subsets2 <- subsets1[,names(subsets1) %in% c("Commodity.Code","Period","Reporter","TradeValue")]
+          data_graph_comtrade <- data_graph_comtrade[,names(data_graph_comtrade) %in% c("Commodity.Code","Period","Reporter","TradeValue")]
         #aggregate the data together, summing over trade values, for groups of period/title pairs
-          subsets2 <- aggregate(subsets2$TradeValue, by = list(subsets2$Period,subsets2$Reporter), FUN = sum, na.rm=TRUE)
+          data_graph_comtrade <- aggregate(data_graph_comtrade$TradeValue, by = list(data_graph_comtrade$Period,data_graph_comtrade$Reporter), FUN = sum, na.rm=TRUE)
           #Reference: https://stackoverflow.com/questions/1660124/how-to-sum-a-variable-by-group
-        names(subsets2)=c("Period","Reporter","TradeValue")
+        names(data_graph_comtrade)=c("Period","Reporter","TradeValue")
           #For some reason, aggregating destroys all the column names so I have to put them back
           
       #add labels next to line
         #Reference: https://statisticsglobe.com/add-labels-at-ends-of-lines-in-ggplot2-line-plot-r
-      data_label <- subsets2
-      data_label$label <-NA
-      data_label$label[which(data_label$Period == max(data_label$Period))] <- data_label$Reporter[which(data_label$Period == max(data_label$Period))]
+      data_graph_comtrade <- data_graph_comtrade
+      data_graph_comtrade$label[which(data_graph_comtrade$Period == max(data_graph_comtrade$Period))] <- data_graph_comtrade$Reporter[which(data_graph_comtrade$Period == max(data_graph_comtrade$Period))]
       
         
       #graph subset
@@ -82,7 +85,7 @@ library("ggrepel")
       print(
         #ggplot won't show up if inside loop without this option
           #https://stackoverflow.com/questions/15678261/ggplot-does-not-work-if-it-is-inside-a-for-loop-although-it-works-outside-of-it
-        ggplot(data_label, mapping = aes(x = Period, y = TradeValue, group=Reporter)) + #group specifies which data should be drawn as a single line
+        ggplot(data_graph_comtrade, mapping = aes(x = Period, y = TradeValue, group=Reporter)) + #group specifies which data should be drawn as a single line
         #adds lines and legend
           geom_line(aes(linetype=Reporter))+
         #geom_lineadds the lines to the graph.
@@ -102,7 +105,7 @@ library("ggrepel")
           #adds the line labels next to the lines
         +theme(legend.position = "none")
             #removes the legend
-        +ylim(0, max(data_label$TradeValue))
+        +ylim(0, max(data_graph_comtrade$TradeValue))
           #https://ggplot2.tidyverse.org/reference/lims.html
           #sets y axis values to range from 0 to whatever the max is
           #without this option, the min y value is set at whatever the min of the data is, not 0
@@ -115,27 +118,145 @@ library("ggrepel")
         #hs_codes_r contain some codes that are 4 digit and some that are 6 digit
         #an HS6 in the trade elascitiy data passes the filter if it its HS6 is an exact match for the HS6 in the code list, or its HS4 is an exact match for an HS4 in the code list
       #keep trade data elasticities that match th filter
-        data_trade_elasticity_subset <- subset(data_trade_elasticity, data_trade_elasticity$filter)
+        data_table_trade_elasticity <- subset(data_trade_elasticity, data_trade_elasticity$filter)
 
   #Create row with average trade elasticity of all( not just trade related) CEPII HSes
-    mean_data <- data.frame("mean of all (not just semi related) HS in CEPII database",0,0,mean(data_trade_elasticity$sigma, na.rm=TRUE),TRUE)
+    mean_TE_of_all_HS <- data.frame("mean of all (not just semi related) HS in CEPII database",0,0,mean(data_trade_elasticity$sigma, na.rm=TRUE),TRUE)
     #https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/mean
     #na.rum =TRUE means it deletes NAs (otherwise it just gives NA for the mean)
-    names(mean_data) <- names(data_trade_elasticity_subset)
+    names(mean_TE_of_all_HS) <- names(data_table_trade_elasticity)
       #this is necessary so that the new row binds with the existing datframe
   #Merge the mean row with the existing dataframe
-    data_trade_elasticity_subset <- rbind(data_trade_elasticity_subset,mean_data)
+    data_table_trade_elasticity <- rbind(data_table_trade_elasticity,mean_TE_of_all_HS)
   #Round to nearest whole number for ease of reading
-    data_trade_elasticity_subset$sigma = round(data_trade_elasticity_subset$sigma,0)
-    data_trade_elasticity_subset
+    data_table_trade_elasticity$sigma = round(data_table_trade_elasticity$sigma,0)
+    data_table_trade_elasticity
   
 #load VLSI Data
-    data_production=read.csv2(file="data/Manual Download/VLSI Production Data.csv",sep=",",header=T,skip=3)
-      #skip=3 to skip irrelevant rows as described in here https://stackoverflow.com/questions/23902421/designating-other-than-first-row-as-headers-in-r
+    data_production=read.csv2(file="data/Manual Download/VLSI Production Data.csv",sep=",",header=T,
+                              skip=3
+                                #skip=3 to skip irrelevant rows as described in here https://stackoverflow.com/questions/23902421/designating-other-than-first-row-as-headers-in-r
+                              #,colClasses = c("character","character","character","numeric","numeric","numeric","numeric","numeric")
+                              ,stringsAsFactors = FALSE
+                              )
+    #Convert VLSI Data to Numeric
     data_production$AOW[is.na(data_production$AOW)]="NA"
       #it imports NA (North America) as <NA> so I turn it back into "NA"
+    #Label Netherlands firms as from NE, not EU
+      data_production$AOW[data_production$COMPANY %in% c("ASML","ASMI")]="NE"
+        #Per Will H's instructions, these are the companies in the Netherlands
+      #see https://cset-collab.atlassian.net/browse/SEMI-41
+    data_production=data_production[data_production$AOW %in% c("NA","JA","NE"),]
+    for (year in 2016:2020){
+      var_name=paste("X",year,sep="")
+        #define corresponding variable name for each year
+      data_production[,var_name][is.na(data_production[,var_name])]="0"
+        #replace NAs with "0"s
+      data_production[,var_name][data_production[,var_name]=="EX"]="0"
+        #replace EXs with "0"s
+      data_production[,var_name]=as.numeric(data_production[,var_name])
+        #convert to numeric
+    }
+
+      
+    #Collapse VLSI Data by HQ Region
+      #delete unused columns
+        data_production <- data_production[,names(data_production) %in% c("AOW","X2016","X2017","X2018","X2019","X2020")]
+      #aggregate the data together, summing over trade values, for each AOW
+        data_production_wide <- aggregate(cbind(data_production$X2016,data_production$X2017,data_production$X2018,data_production$X2019,data_production$X2020),
+                                           by = list(data_production$AOW), FUN = sum, na.rm=TRUE)
+          #Reference: https://stackoverflow.com/questions/1660124/how-to-sum-a-variable-by-group
+      #For some reason, aggregating destroys all the column names so I have to put them back
+        names(data_production_wide)=c("Region","X2016","X2017","X2018","X2019","X2020")
     
-#Combine VLSI Data with Comtrade Data
-    #NB: VLSI data is in millions
-    
-#Graph VLSI Data
+      #Reshape data to long
+        library(tidyr)
+        #convert ID variable to factor
+          #specified here http://www.cookbook-r.com/Manipulating_data/Converting_data_between_wide_and_long_format/
+          data_production_wide$Region=as.factor(data_production_wide$Region)
+        #Do reshaping
+          #http://www.cookbook-r.com/Manipulating_data/Converting_data_between_wide_and_long_format/
+          data_production_long <- gather(data_production_wide, Year, Production, X2016:X2020, factor_key=TRUE)
+        
+        #Convert years to numeric  
+          library(stringr)
+          data_production_long$Year=as.numeric(str_sub(data_production_long$Year,-4))
+        
+        #Convert report names to "country" to prep for merge with produciton data
+          names(data_production_long)[names(data_production_long)=="Region"]="Country"
+        #Convert country names to long names to prep for merge
+          levels(data_production_long$Country)[levels(data_production_long$Country)=="JA"]="Japan"
+          levels(data_production_long$Country)[levels(data_production_long$Country)=="NE"]="Netherlands"
+          levels(data_production_long$Country)[levels(data_production_long$Country)=="NA"]="USA"
+          
+          
+      #Create COMTRADE Dataset by country
+        #delete all data not from 3 important countries
+            data_graph_comtrade_country=data_comtrade[(data_comtrade$Reporter %in% c("Japan","USA","Netherlands")),]
+          #Delete all columns except for the ones I am using
+            data_graph_comtrade_country <- data_graph_comtrade_country[,names(data_graph_comtrade_country) %in% c("Period","Reporter","TradeValue")]
+          #aggregate the data together, summing over trade values, for groups of period/title pairs
+            data_graph_comtrade_country <- aggregate(data_graph_comtrade_country$TradeValue, by = list(data_graph_comtrade_country$Period,data_graph_comtrade_country$Reporter), FUN = sum, na.rm=TRUE)
+            #Reference: https://stackoverflow.com/questions/1660124/how-to-sum-a-variable-by-group
+          names(data_graph_comtrade_country)=c("Year","Country","Exports")
+            #For some reason, aggregating destroys all the column names so I have to put them back
+          #Convert Country to factor
+            data_graph_comtrade_country$Country=as.factor(data_graph_comtrade_country$Country)
+      
+      #Merge production and comtrade data
+          data_pe_wide=merge(data_graph_comtrade_country,data_production_long,by=c("Country","Year"))
+          #Convert data to all be billions
+            data_pe_wide$Exports=data_pe_wide$Exports/1000/1000/1000 #Exports are originally in dollars
+            data_pe_wide$Production=data_pe_wide$Production/1000  #Production originally in million dollars
+          #Reshape dataset to long
+            #http://www.cookbook-r.com/Manipulating_data/Converting_data_between_wide_and_long_format/
+            data_pe_long <- gather(data_pe_wide, Flow, Value, Production:Exports, factor_key=TRUE)
+            
+            
+      #Graph Production and Export Data by Country
+            
+            #Convert Flow from factor to character
+              #https://stackoverflow.com/questions/2851015/convert-data-frame-columns-from-factors-to-characters
+              library(dplyr)
+              data_pe_long %>% mutate(across(where(is.factor), as.character)) -> data_pe_long
+            
+            
+              #add labels next to line, at the last year, of each production type
+                #Reference: https://statisticsglobe.com/add-labels-at-ends-of-lines-in-ggplot2-line-plot-r
+                data_pe_long$label[which(data_pe_long$Year == max(data_pe_long$Year))] <-
+                  data_pe_long$Flow[which(data_pe_long$Year == max(data_pe_long$Year))]
+              
+                #graph each country
+                list_regions=c("USA","Netherlands","Japan")
+                for (i in 1:length(list_regions)) {
+                  # From https://www.datanovia.com/en/blog/how-to-subset-a-dataset-when-plotting-with-ggplot2/
+                  print(
+                    #ggplot won't show up if inside loop without this option
+                    #https://stackoverflow.com/questions/15678261/ggplot-does-not-work-if-it-is-inside-a-for-loop-although-it-works-outside-of-it
+                    ggplot(data_pe_long[data_pe_long$Country==list_regions[i],], mapping = aes(x = Year, y = Value, group=Flow)) + #group specifies which data should be drawn as a single line
+                      #adds lines and legend
+                      geom_line(aes(linetype=Flow))+
+                      #geom_lineadds the lines to the graph.
+                      #linetype specification adds teh legend
+                      #add big points (scatterplot)
+                      #reference: http://www.sthda.com/english/wiki/ggplot2-line-plot-quick-start-guide-r-software-and-data-visualization  
+                      geom_point()+ 
+                      #Label title and axis
+                      #Reference: http://www.sthda.com/english/wiki/ggplot2-line-plot-quick-start-guide-r-software-and-data-visualization#customized-line-graphs
+                      labs(title=list_regions[i]
+                           ,x="time"
+                           ,y="Billion USD"
+                      )+
+                      geom_label_repel(aes(label = label),
+                                       nudge_x = 1,
+                                       na.rm = TRUE)
+                    #adds the line labels next to the lines
+                    +theme(legend.position = "none")
+                    #removes the legend
+                    +ylim(0, max(data_pe_long$Value))
+                    #https://ggplot2.tidyverse.org/reference/lims.html
+                    #sets y axis values to range from 0 to whatever the max is
+                    #without this option, the min y value is set at whatever the min of the data is, not 0
+                  )
+                }
+
